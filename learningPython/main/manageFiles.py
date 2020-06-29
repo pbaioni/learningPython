ceee6e70f042b77190a2be53ae4d26a3c6e517da
paolobaioni <paolobaioni@localhost>
2020-06-29 11:51:17 +0200

    use try in manageFiles

--- learningPython/main/manageFiles.py
+++ learningPython/main/manageFiles.py
@@ -1,26 +1,34 @@
 import os
 
 def readFileContent(filename):
-    f = open(filename,"r")
-    content = f.read()
-    f.close()
+    try:
+        f = open(filename,"r")
+        content = f.read()
+    finally:
+        f.close()
     return content
     
 def appendFileContent(filename, content):
-    f = open(filename,"a")
-    f.write("\n" + content)
-    f.close()
+    try:
+        f = open(filename,"a")
+        f.write("\n" + content)
+    finally:
+        f.close()
     return
 
 def writeFile(filename, content):
-    f = open(filename,"w+")
-    f.write(content)
-    f.close()
+    try:
+        f = open(filename,"w+")
+        f.write(content)
+    finally:
+        f.close()
     return
 
 def createFile(filename):
-    f = open(filename,"w+")
-    f.close()
+    try:
+        f = open(filename,"w+")
+    finally:
+        f.close()
     return 
 
 def deleteFile(filename):
